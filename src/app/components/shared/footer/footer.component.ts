d3610fe8eab0c8b540476045aa3da7fc26f59d67
Rony Cifuentes <ronycif8597@gmail.com>
2022-09-22 23:39:36 -0600

    like-deslik-actions-button-footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class FooterComponent implements OnInit {
+  
+  likeCount: number=0;
+  deslikeCount: number=0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  like(): void {
+    this.likeCount+=1;
+    if (this.deslikeCount >0){
+      this.deslikeCount=0;
+    } 
+    console.log("Like",this.likeCount)
+  }
+
+  deslike(): void {
+    this.deslikeCount+=1;
+    if (this.likeCount >0){
+      this.likeCount=0;
+    } 
+    console.log("Deslike",this.deslikeCount)
   }
 
 }
